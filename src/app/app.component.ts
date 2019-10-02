344a4b58a3657fff81cea4eb66f8cdcc9a4baabd
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2019-10-02 11:55:22 +0200

    :lipstick: set indent to 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'fld-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'fld-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'unicorn-ng';
+    title = 'unicorn-ng';
 }
