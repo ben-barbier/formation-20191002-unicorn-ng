62e619cba8655d8117c36afbe5f2195f54fb94a0
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2019-10-03 11:17:25 +0200

    :lipstick: improve unicorn card style

--- src/app/shared/modules/material.module.ts
+++ src/app/shared/modules/material.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { MatIconModule } from '@angular/material';
+import { MatCardModule, MatIconModule } from '@angular/material';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
@@ -14,6 +14,7 @@ import { MatListModule } from '@angular/material/list';
         MatButtonModule,
         MatSidenavModule,
         MatListModule,
+        MatCardModule,
     ],
 })
 export class MaterialModule {
